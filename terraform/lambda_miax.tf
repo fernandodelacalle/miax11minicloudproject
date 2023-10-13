dae8dac0948d890caef0a97bf374eed49dead9db
Fernando de la Calle Silos <fernando.delacalle@six-group.com>
2023-10-13 23:26:48 +0200

    add log to lambda

--- terraform/lambda_miax.tf
+++ terraform/lambda_miax.tf
@@ -45,9 +45,35 @@ resource "aws_iam_role_policy_attachment" "test-attach" {
   policy_arn = aws_iam_policy.lamda_policy.arn
 }
 
+
+data "aws_iam_policy_document" "lambda_logging" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = ["arn:aws:logs:*:*:*"]
+  }
+}
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "lambda_logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+  policy      = data.aws_iam_policy_document.lambda_logging.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_execution_role.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
+}
+
 resource "aws_lambda_function" "executable" {
   function_name = "test_terraform"
-  image_uri     = "076977333390.dkr.ecr.eu-west-1.amazonaws.com/miax11minicloudprojectlambda:a856a9c6400e65c1a8d8f41b448200e9c9dcc77b"
+  image_uri     = "076977333390.dkr.ecr.eu-west-1.amazonaws.com/miax11minicloudprojectlambda:0a67778ee9ff17e1dcbe56f5e0f26809debb9935"
   package_type  = "Image"
   role          = aws_iam_role.lambda_execution_role.arn
   memory_size = 1024
